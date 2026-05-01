8176717923fd950400da020c80e9deae6fc71149
Matheus <matheus.mathias2002@gmail.com>
2026-05-01 20:35:00 -0300

    ex_03 da aula_02 refeito

---
+++ aula_02/ex_03/refazendo.c
@@ -0,0 +1,50 @@
+#include "vector.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Vector *vector_construct(){
+    Vector *vector = (Vector*)calloc(1, sizeof(Vector));
+    vector->size = 0;
+    vector->allocated = 10;
+    vector->data = (data_type*)calloc(vector->allocated, sizeof(data_type));
+
+    return vector;
+}
+
+void vector_push_back(Vector *v, data_type val){
+    if (v->size >= v->allocated){
+        v->allocated *= 2;
+        v->data = (data_type *)realloc(v->data, v->allocated * sizeof(data_type));
+    }
+    v->data[v->size] = val;
+    v->size++;
+}
+
+data_type vector_get(Vector *v, int i){
+    if (i < 0 || i >= v->size){
+        exit(EXIT_FAILURE); 
+    } else {
+        return v->data[i];
+    }
+}
+void vector_set(Vector *v, int i, data_type val){
+    if (i < 0 || i >= v->size){
+        exit(EXIT_FAILURE);
+    } else {
+        v->data[i] = val;
+    }
+}
+int vector_size(Vector *v){
+    return v->size;
+}
+
+void vector_destroy(Vector *v){
+    if (v != NULL){
+        if (v->data != NULL){
+            free(v->data);
+        }
+        free(v);
+    }
+}
+
