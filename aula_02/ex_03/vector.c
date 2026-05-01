936a788f54803462b1c0989574e441f2b4cf0225
Matheus <matheus.mathias2002@gmail.com>
2026-05-01 20:46:10 -0300

    ex_04 da aula_02 refeito

--- aula_02/ex_03/vector.c
+++ aula_02/ex_03/vector.c
@@ -4,39 +4,33 @@
 #include <string.h>
 
 Vector *vector_construct(){
+    Vector *vector = (Vector*)calloc(1, sizeof(Vector));
+    vector->size = 0;
+    vector->allocated = 10;
+    vector->data = (data_type*)calloc(vector->allocated, sizeof(data_type));
 
-    Vector *v = (Vector *)calloc(1, sizeof(Vector));
-    v->size = 0;
-    v->allocated = 10;
-    v->data = (data_type *)calloc(v->allocated, sizeof(data_type));
-
-    return v;
-    
+    return vector;
 }
 
 void vector_push_back(Vector *v, data_type val){
     if (v->size >= v->allocated){
-        int new_allocated = 2 * v->allocated;
-
-        data_type *new_data = (data_type *)realloc(v->data, new_allocated * sizeof(data_type));
-
-        v->data = new_data;
-        v->allocated = new_allocated;
+        v->allocated *= 2;
+        v->data = (data_type *)realloc(v->data, v->allocated * sizeof(data_type));
     }
     v->data[v->size] = val;
     v->size++;
 }
 
 data_type vector_get(Vector *v, int i){
-    if (i > v->size || i < 0){
-        exit(EXIT_SUCCESS);
+    if (i < 0 || i >= v->size){
+        exit(EXIT_FAILURE); 
     } else {
         return v->data[i];
     }
 }
 void vector_set(Vector *v, int i, data_type val){
-    if (i > v->size || i < 0){
-        exit(EXIT_SUCCESS);
+    if (i < 0 || i >= v->size){
+        exit(EXIT_FAILURE);
     } else {
         v->data[i] = val;
     }
@@ -45,12 +39,12 @@ int vector_size(Vector *v){
     return v->size;
 }
 
-
 void vector_destroy(Vector *v){
-    if (!v){
-        for (int i = 0; i < v->size; i++){
+    if (v != NULL){
+        if (v->data != NULL){
             free(v->data);
         }
         free(v);
     }
 }
+
--- aula_02/ex_04/vector.c
+++ aula_02/ex_04/vector.c
@@ -1,75 +1,38 @@
+#include "vector.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "vector.h"
-
+#include <string.h>
 
 Vector *vector_construct(){
+    Vector *vector = (Vector*)calloc(1, sizeof(Vector));
+    vector->size = 0;
+    vector->allocated = 10;
+    vector->data = (data_type*)calloc(vector->allocated, sizeof(data_type));
 
-    Vector *v = (Vector *)calloc(1, sizeof(Vector));
-    v->size = 0;
-    v->allocated = 10;
-    v->data = (data_type *)calloc(v->allocated, sizeof(data_type));
-
-    return v;
-    
+    return vector;
 }
 
 void vector_push_back(Vector *v, data_type val){
     if (v->size >= v->allocated){
-        int new_allocated = 2 * v->allocated;
-
-        data_type *new_data = (data_type *)realloc(v->data, new_allocated * sizeof(data_type));
-
-        v->data = new_data;
-        v->allocated = new_allocated;
+        v->allocated *= 2;
+        v->data = (data_type *)realloc(v->data, v->allocated * sizeof(data_type));
     }
     v->data[v->size] = val;
     v->size++;
 }
 
 data_type vector_get(Vector *v, int i){
-    if (i > v->size || i < 0){
-        exit(EXIT_SUCCESS);
+    if (i < 0 || i >= v->size){
+        exit(EXIT_FAILURE); 
     } else {
         return v->data[i];
     }
 }
 void vector_set(Vector *v, int i, data_type val){
-    if (i > v->size || i < 0){
-        exit(EXIT_SUCCESS);
+    if (i < 0 || i >= v->size){
+        exit(EXIT_FAILURE);
     } else {
         v->data[i] = val;
-    }
-}
-int vector_size(Vector *v){
-    return v->size;
-}
-
-
-void vector_destroy(Vector *v){
-    if (!v){
-        for (int i = 0; i < v->size; i++){
-            free(v->data);
-        }
-        free(v);
-    }
-}
-
-
-data_type vector_get(Vector *v, int i){
-    if (i >= 0 && i < v->size){
-        return v->data[i];
-    } else {
-        //printf("Erro: Nao ha valor definido para o vetor na posicao %d.\n", i);
-        exit(EXIT_SUCCESS);
-    }
-}
-void vector_set(Vector *v, int i, data_type val){
-    if (i >= 0 && i < v->size){
-        v->data[i] = val;
-    } else {
-        //printf("Erro: Nao ha valor definido para o vetor na posicao %d.\n", i);
-        exit(EXIT_SUCCESS);
     }
 }
 int vector_size(Vector *v){
@@ -77,7 +40,7 @@ int vector_size(Vector *v){
 }
 
 int vector_find(Vector *v, data_type val){
-    for (int i = 0; i < v->data; i++){
+    for (int i = 0; i < v->size; i++){
         if (v->data[i] == val){
             return i;
         }
@@ -85,11 +48,12 @@ int vector_find(Vector *v, data_type val){
     return -1;
 }
 
-
-void vector_destroy(Vector *v) {
-    if (v == NULL) return;
-    if (v->data != NULL) {
-        free(v->data);
+void vector_destroy(Vector *v){
+    if (v != NULL){
+        if (v->data != NULL){
+            free(v->data);
+        }
+        free(v);
     }
-    free(v);
 }
+
