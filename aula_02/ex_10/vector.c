a45af442d6a518b3854ea81b08fcc41ddff4eeaf
Matheus <matheus.mathias2002@gmail.com>
2026-05-04 12:37:15 -0300

    aula_02 ex_10 refeito

--- aula_02/ex_10/vector.c
+++ aula_02/ex_10/vector.c
@@ -1,50 +1,38 @@
+#include "vector.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "vector.h"
+#include <string.h>
 
-Vector* vector_construct() {
-    Vector *v = (Vector*) calloc(1, sizeof(Vector));
-    if (v == NULL) return NULL;
+Vector *vector_construct(){
+    Vector *vector = (Vector*)calloc(1, sizeof(Vector));
+    vector->size = 0;
+    vector->allocated = 10;
+    vector->data = (data_type*)calloc(vector->allocated, sizeof(data_type));
 
-    v->allocated = 10; 
-    v->size = 0;      
-    v->data = (data_type*) calloc(v->allocated, sizeof(data_type));
-
-    if (v->data == NULL) {
-        free(v);
-        return NULL;
-    }
-    return v;
+    return vector;
 }
 
-void vector_push_back(Vector *v, data_type val) {
-    if (v->size == v->allocated) {
-        int new_allocated = v->allocated * 2;
-        data_type *temp = (data_type*) realloc(v->data, new_allocated * sizeof(data_type));
-        
-        if (temp == NULL) return; 
-
-        v->data = temp;
-        v->allocated = new_allocated;
+void vector_push_back(Vector *v, data_type val){
+    if (v->size >= v->allocated){
+        v->allocated *= 2;
+        v->data = (data_type *)realloc(v->data, v->allocated * sizeof(data_type));
     }
     v->data[v->size] = val;
     v->size++;
 }
 
 data_type vector_get(Vector *v, int i){
-    if (i >= 0 && i < v->size){
-        return v->data[i];
+    if (i < 0 || i >= v->size){
+        exit(EXIT_FAILURE); 
     } else {
-        //printf("Erro: Nao ha valor definido para o vetor na posicao %d.\n", i);
-        exit(EXIT_SUCCESS);
+        return v->data[i];
     }
 }
 void vector_set(Vector *v, int i, data_type val){
-    if (i >= 0 && i < v->size){
-        v->data[i] = val;
+    if (i < 0 || i >= v->size){
+        exit(EXIT_FAILURE);
     } else {
-        //printf("Erro: Nao ha valor definido para o vetor na posicao %d.\n", i);
-        exit(EXIT_SUCCESS);
+        v->data[i] = val;
     }
 }
 int vector_size(Vector *v){
@@ -93,83 +81,68 @@ data_type vector_min(Vector *v){
 // Retorna o Ã­ndice do maior elemento do vector (assumindo que podemos comparar elementos usando o operador "<")
 int vector_argmax(Vector *v){
     int maiorElemento = vector_max(v);
-
     return vector_find(v, maiorElemento);
 }
 
 // Retorna o Ã­ndice do menor elemento do vector (assumindo que podemos comparar elementos usando o operador "<")
 int vector_argmin(Vector *v){
     int menorElemento = vector_min(v);
-
     return vector_find(v, menorElemento);
 }
 
-// Remove o i-Ã©simo elemento do vetor.
-data_type vector_remove(Vector *v, int i){
-    data_type item_removido = v->data[i];
+void vector_swap(Vector *v, int i, int j){
+    data_type val;
+    val = v->data[i];
+    v->data[i] = v->data[j];
+    v->data[j] = val;
+}
 
-    for(int j = i; j < v->size - 1; j++){
+data_type vector_remove(Vector *v, int i){
+    data_type removido = v->data[i];
+
+    for (int j = i; j < v->size - 1; j++){
         v->data[j] = v->data[j + 1];
     }
-
     v->size--;
+    return removido;
+}
 
-    return item_removido;
+void vector_insert(Vector *v, int i, data_type val){
+    if (v->size >= v->allocated){
+        v->allocated *= 2;
+        v->data = (data_type*)realloc(v->data, v->allocated * sizeof(data_type));
+    }
+    for (int j = v->size - 1; j >= i; j--){
+        v->data[j + 1] = v->data[j];
+    }
+    v->data[i] = val;
+    v->size++;
 }
 
 // Remove o primeiro elemento
 data_type vector_pop_front(Vector *v){
-    return vector_remove(v, 0);
+    data_type removido = vector_remove(v, 0);
+    return removido;
 }
 
 // Remove o ultimo elemento
 data_type vector_pop_back(Vector *v){
-    return vector_remove(v, v->size);
+    data_type removido = vector_remove(v, v->size - 1);
+    return removido;
 }
 
-// Insere o elemento na i-esima posicao
-void vector_insert(Vector *v, int i, data_type val){
-    if (v->allocated == v->size){
-        v->data = (data_type *) realloc(v->data, 2 * sizeof(data_type));
-    }
-    for (int j = v->size; j > i; j--){
-        v->data[j] = v->data[j - 1];
-    }
-
-    v->data[i] = val;
-
-    v->size++;
-}
-
-// Troca os elementos das posiÃ§Ãµes i e j (i vira j e j vira i)
-void vector_swap(Vector *v, int i, int j){
-    int aux;
-    aux = v->data[i];
-    v->data[i] = v->data[j];
-    v->data[j] = aux;
-}
-
-// Inverte o vetor in-place (sem criar um novo vetor)
 void vector_reverse(Vector *v){
-    int dec = v->size - 1;
-    for (int i = 0; i < v->size / 2; i++){
-        vector_swap(v, i, dec);
-        dec--;
+    for (int i = 0; i < (v->size / 2); i++){
+        vector_swap(v, i, v->size - i - 1);
     }
-
-    /* código recomendado:
-    for (int i = 0; i < v->size / 2; i++) {
-        // (v->size - 1 - i) é o par exato do índice i
-        vector_swap(v, i, v->size - 1 - i);
-    }
-    */
 }
 
-
-void vector_destroy(Vector *v) {
-    if (v == NULL) return;
-    if (v->data != NULL) {
-        free(v->data);
+void vector_destroy(Vector *v){
+    if (v != NULL){
+        if (v->data != NULL){
+            free(v->data);
+        }
+        free(v);
     }
-    free(v);
 }
+
